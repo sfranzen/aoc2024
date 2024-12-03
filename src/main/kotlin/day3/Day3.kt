1ef72a87192057b49a8b7c084e34dab8ef7a28d5
Steven Franzen <s.franzen@belastingdienst.nl>
2024-12-04 00:22:20 +0100

    Day 3

---
+++ src/main/kotlin/day3/Day3.kt
@@ -0,0 +1,31 @@
+﻿package day3
+
+import getInput
+
+val input = getInput(3)
+
+fun String.trySplit(delimiter: String) = split(delimiter, limit = 2).let { it[0] to it.getOrElse(1) { "" } }
+
+fun part1(input: String): UInt = """mul\((\d+),(\d+)\)""".toRegex().findAll(input).sumOf {
+    it.groupValues.drop(1).map(String::toUInt).reduce(UInt::times)
+}
+
+fun part2(input: String, enabled: Boolean = true): UInt {
+    val activate = "do()"
+    val deactivate = "don't()"
+
+    if (input.isBlank()) return 0u
+
+    if (enabled) {
+        val (head, tail) = input.trySplit(deactivate)
+        return part1(head) + part2(tail, false)
+    }
+
+    val (_, tail) = input.trySplit(activate)
+    return part2(tail)
+}
+
+fun main() {
+    println(input.readLines().sumOf(::part1))
+    println(input.readText().let(::part2))
+}
---
+++ src/test/kotlin/Day3Test.kt
@@ -0,0 +1,19 @@
+﻿import day3.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day3Test {
+    @Test
+    fun part1() {
+        val input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        val expected = 161u
+        assertEquals(expected, part1(input))
+    }
+
+    @Test
+    fun part2() {
+        val input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        val expected = 48u
+        assertEquals(expected, part2(input))
+    }
+}
