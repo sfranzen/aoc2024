da60e0ef144d43d43c0720ae5f203c67c435c37d
Steven Franzen <s.franzen@belastingdienst.nl>
2024-12-05 18:28:44 +0100

    Day 5, part 1

---
+++ src/main/kotlin/day5/Day5.kt
@@ -0,0 +1,32 @@
+﻿package day5
+
+import getInput
+
+typealias Rule = List<Int>
+typealias Update = List<Int>
+
+val input = getInput(5).readLines()
+
+fun process(input: List<String>): Pair<RuleSet, List<Update>> =
+    input.filter(String::isNotBlank).partition { it.contains('|') }.run {
+        first.map { it.split('|').map(String::toInt) }.let(::RuleSet) to second.map { it.split(',').map(String::toInt) }
+    }
+
+data class RuleSet(val rules: List<Rule>) : List<Rule> by rules {
+    fun validate(update: Update): Boolean = matchingRules(update).all {
+        it.map { page -> update.indexOf(page) }.windowed(2).all { (a, b) -> a < b }
+    }
+
+    private fun matchingRules(update: Update): List<Rule> = rules.filter { update.containsAll(it) }
+}
+
+fun part1(input: List<String>): Int {
+    val (ruleSet, updates) = process(input)
+    return updates.filter { ruleSet.validate(it) }.sumOf { it[it.size / 2] }
+}
+
+fun part2(input: List<String>) = 0
+
+fun main() {
+    println(part1(input))
+}
---
+++ src/test/kotlin/day5/Day5Test.kt
@@ -0,0 +1,77 @@
+﻿package day5
+
+import org.junit.jupiter.api.Test
+
+import kotlin.test.assertEquals
+
+class Day5Test {
+    private val testInput = """
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
+    """.trimIndent().split('\n')
+
+    @Test
+    fun process() {
+        val input = """
+            47|53
+            97|13
+            
+            75,47
+            97,61
+        """.trimIndent().split('\n')
+        val expectedRules = listOf(
+            listOf(47, 53), listOf(97, 13)
+        )
+        val expectedUpdates = listOf(
+            listOf(75, 47), listOf(97, 61)
+        )
+
+        val (rules, updates) = process(input)
+
+        assertEquals(expectedRules, rules)
+        assertEquals(expectedUpdates, updates)
+    }
+
+    @Test
+    fun validation() {
+        val expected = listOf(true, true, true, false, false, false)
+        val (sut, updates) = process(testInput)
+
+        assertEquals(expected, updates.map { sut.validate(it) })
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(143, part1(testInput))
+    }
+
+    @Test
+    fun part2() {
+    }
+}
