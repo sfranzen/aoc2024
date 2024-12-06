d7378ae8965755135fac7b7481c5cbd26e3b876b
Steven Franzen <s.franzen@belastingdienst.nl>
2024-12-06 22:01:53 +0100

    Day 6, part 1

---
+++ src/main/kotlin/day6/Day6.kt
@@ -0,0 +1,65 @@
+﻿package day6
+
+import getInput
+
+class Room(private val layout: List<String>) {
+    private val guard = findGuard(layout)
+    private val visited = mutableSetOf(guard.position)
+    private var finished = false
+
+    fun predict(): Set<Position> {
+        do update()
+        while (!finished)
+        return visited
+    }
+
+    private fun update() {
+        if (finished) return
+
+        val nextPos = guard.nextPosition()
+        when (inspect(nextPos)) {
+            null -> finished = true
+            '#' -> guard.turn()
+            else -> {
+                guard.position = nextPos
+                visited.add(nextPos)
+            }
+        }
+    }
+
+    private fun inspect(pos: Position) = with(pos) { layout.getOrNull(row)?.getOrNull(col) }
+}
+
+data class Guard(var position: Position, var direction: Direction) {
+    fun nextPosition() = with(position) {
+        when (direction) {
+            Direction.Up -> Position(row - 1, col)
+            Direction.Right -> Position(row, col + 1)
+            Direction.Down -> Position(row + 1, col)
+            Direction.Left -> Position(row, col - 1)
+        }
+    }
+
+    fun turn() {
+        direction = direction.run { Direction.entries[(ordinal + 1) % Direction.entries.size] }
+    }
+}
+
+data class Position(val row: Int, val col: Int)
+
+enum class Direction(val symbol: Char) {
+    Up('^'), Right('>'), Down('v'), Left('<')
+}
+
+fun findGuard(layout: List<String>): Guard = layout.flatMapIndexed { rowIndex, row ->
+    row.mapIndexedNotNull { colIndex, symbol ->
+        Direction.entries.find { it.symbol == symbol }?.let { Guard(Position(rowIndex, colIndex), it) }
+    }
+}.single()
+
+fun part1(input: List<String>) = Room(input).predict().size
+
+fun main() {
+    val input = getInput(6)
+    println(part1(input))
+}
---
+++ src/test/kotlin/day6/Day6Test.kt
@@ -0,0 +1,52 @@
+﻿package day6
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class Day6Test {
+    private val testInput = """
+        ....#.....
+        .........#
+        ..........
+        ..#.......
+        .......#..
+        ..........
+        .#..^.....
+        ........#.
+        #.........
+        ......#...
+    """.trimIndent().split('\n')
+
+    @Test
+    fun `turn() turns the guard to the right`() {
+        val sut = Guard(Position(0, 0), Direction.Left)
+        val expected = Direction.entries
+
+        expected.forEach {
+            sut.turn()
+            assertEquals(it, sut.direction)
+        }
+    }
+
+    @Test
+    fun `findGuard() returns the single guard when present`() {
+        val expected = Guard(Position(6, 4), Direction.Up)
+        assertEquals(expected, findGuard(testInput))
+    }
+
+    @Test
+    fun `findGuard() throws when no guard is present`() {
+        assertThrows<NoSuchElementException> { findGuard(listOf("")) }
+    }
+
+    @Test
+    fun `findGuard() throws when more than one guard is present`() {
+        assertThrows<IllegalArgumentException> { findGuard(listOf("^^")) }
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(41, part1(testInput))
+    }
+}
