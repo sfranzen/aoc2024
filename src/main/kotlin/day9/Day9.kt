e477d120f2cb2d370f26ec397a98c2c4189435c8
Steven Franzen <s.franzen@belastingdienst.nl>
2024-12-10 21:33:04 +0100

    Day 9, part 1

---
+++ src/main/kotlin/day9/Day9.kt
@@ -0,0 +1,45 @@
+﻿package day9
+
+import getInput
+
+data class Block(val id: ULong? = null) {
+    fun isEmpty() = id == null
+    override fun toString() = id?.toString() ?: "."
+}
+
+class Disk(input: String) {
+    private val filesystem = buildList {
+        input.forEachIndexed { index, char ->
+            val block = when (index % 2) {
+                0 -> Block(index.toULong() / 2u)
+                else -> Block()
+            }
+            repeat(char.digitToInt()) { add(block) }
+        }
+    }.toMutableList()
+
+    private val occupied = filesystem.count { !it.isEmpty() }
+
+    fun defragment() {
+        val sources = filesystem.asReversed().asSequence().filterNot(Block::isEmpty).iterator()
+        filesystem.take(occupied).forEachIndexed { index, target ->
+            if (target.isEmpty()) {
+                filesystem[index] = sources.next()
+            }
+        }
+        filesystem.indices.drop(occupied).forEach { filesystem[it] = Block() }
+    }
+
+    fun checksum(): ULong = filesystem.withIndex().filterNot { it.value.isEmpty() }
+        .sumOf { (index, block) -> index.toULong() * (block.id ?: 0u) }
+
+    override fun toString() = filesystem.joinToString("")
+}
+
+fun part1(input: String) = Disk(input).apply(Disk::defragment).checksum()
+fun part2(input: String) = 0
+
+fun main() {
+    val input = getInput(9).first()
+    println(part1(input))
+}
---
+++ src/test/kotlin/day9/Day9Test.kt
@@ -0,0 +1,34 @@
+﻿package day9
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.assertEquals
+
+class Day9Test {
+    private val testInput = "2333133121414131402"
+
+    @Test
+    fun disk() {
+        val sut = Disk(testInput)
+        val expectedLayout = "00...111...2...333.44.5555.6666.777.888899"
+        assertEquals(expectedLayout, sut.toString())
+    }
+
+    @Test
+    fun defragment() {
+        val sut = Disk(testInput)
+        val expectedLayout = "0099811188827773336446555566.............."
+        sut.defragment()
+        assertEquals(expectedLayout, sut.toString())
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(1928u, part1(testInput))
+    }
+
+    @Test
+    fun part2() {
+    }
+}
