f25f9d534047f6077ddaa91db8713e8e87576747
Steven Franzen <sfranzen85@gmail.com>
2024-12-19 23:21:28 +0100

    Move input files

--- src/main/kotlin/Input.kt
+++ src/main/kotlin/Input.kt
@@ -1,7 +1,7 @@
 import java.io.FileNotFoundException
 
 fun getInput(day: Int): List<String> {
-    val resource = "/day$day/input"
+    val resource = "/day$day.txt"
     return object {}.javaClass.getResourceAsStream(resource)
         ?.run { readAllBytes().decodeToString().split('\n').dropLastWhile { it.isBlank() } }
         ?: throw FileNotFoundException(resource)
